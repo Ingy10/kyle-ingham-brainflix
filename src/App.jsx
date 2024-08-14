432cfd925153eb626ea8bdfaaca4a18190aed139
Kyle Ingham <kyleingy@proton.me>
2024-08-14 15:24:10 -0600

    created files for footer section.

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import "./App.scss";
 import Header from "../src/components/Header/Header";
 import Main from "../src/components/Main/Main";
 import VideoData from "../src/components/VideoData/VideoData";
+import Footer from "../src/components/Footer/Footer";
 
 function App() {
   return (
     <section className="page">
       <Header />
       <Main />
-      {/* <VideoData /> */}
+      <Footer />
     </section>
   );
 }
