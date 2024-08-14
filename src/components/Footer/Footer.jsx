36dd9ec562df2589d9f80003d57314ed80938adc
Kyle Ingham <kyleingy@proton.me>
2024-08-14 16:42:21 -0600

    Used map to add videos to video list in footer section and inserted relevant information into the html.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,24 +1,32 @@
 import "./Footer.scss";
-import Thumbnail from "../../assets/images/Upload-video-preview.jpg";
+import VideoData from "../VideoData/VideoData";
 
-function videoList() {
+function VideoList() {
+  const videos = VideoData();
+
   return (
     <>
       <section className="video-list">
         <h2 className="video-list__title">Next Videos</h2>
         <ul className="video-list__container">
-          <li className="video-list__item">
-            <div className="video-list__thumbnail-container">
-              <img className="video-list__thumbnail-image" src={Thumbnail} />
-            </div>
-            <div className="video-list__content-container">
-              <p className="video-list__video-title">Title of Video</p>
-              <p className="video-list__video-author">Authors name</p>
-            </div>
-          </li>
+          {videos.map((video) => (
+            <li className="video-list__item" key={video.id}>
+              <div className="video-list__thumbnail-container">
+                <img
+                  className="video-list__thumbnail-image"
+                  src={video.image}
+                  alt={video.title}
+                />
+              </div>
+              <div className="video-list__content-container">
+                <p className="video-list__video-title">{video.title}</p>
+                <p className="video-list__video-author">{video.channel}</p>
+              </div>
+            </li>
+          ))}
         </ul>
       </section>
     </>
   );
 }
-export default videoList;
+export default VideoList;
