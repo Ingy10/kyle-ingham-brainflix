2ac0ec60d890513358ebcdf6c74bd823f1b54347
Kyle Ingham <kyleingy@proton.me>
2024-08-14 11:11:30 -0600

    created and styled comment form and top of comment section with submit button. Created more mixins for avatar container and image.

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,7 +1,36 @@
 import "./Comments.scss";
+import profileImage from "../../assets/images/Mohan-muruge.jpg";
+import commentIcon from "../../assets/icons/add_comment.svg";
 
 function Comments() {
-  return <></>;
+  return (
+    <>
+      <section className="comments">
+        <div className="comments__user-avatar">
+          <div className="comments__user-avatar--container">
+            <img className="comments__user-avatar--img" src={profileImage} />
+          </div>
+        </div>
+        <form className="comments__form">
+          <label className="comments__form-title" name="comment">
+            JOIN THE CONVERSATION
+          </label>
+          <textarea
+            className="comments__form-input"
+            id="comment"
+            rows="7"
+            cols="30"
+          ></textarea>
+          <div className="comments__button-wrapper">
+            <button className="comments__form-button " type="submit">
+              COMMENT
+            </button>
+            <img className="comments__form-button--icon" src={commentIcon} />
+          </div>
+        </form>
+      </section>
+    </>
+  );
 }
 
 export default Comments;
