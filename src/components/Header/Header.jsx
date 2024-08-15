0a29888fe07b47ce20957d478a914c0a5a2eabf6
Kyle Ingham <kyleingy@proton.me>
2024-08-14 18:13:54 -0600

    Added button icons to video player.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,25 @@
 import "./Header.scss";
 import NavBar from "../NavBar/NavBar";
 import Video from "../../assets/images/Upload-video-preview.jpg";
+import PlayIcon from "../../assets/icons/play.svg";
+import FullScreen from "../../assets/icons/fullscreen.svg";
+import VolumeOff from "../../assets/icons/volume_up.svg";
 
 function Header() {
   return (
     <>
       <header className="header">
         <NavBar />
-        <video
-          className="header__video-player"
-          src={Video}
-          poster={Video}
-        ></video>
+        <div class="header__video-wrapper">
+          <video
+            className="header__video-player"
+            src={Video}
+            poster={Video}
+          ></video>
+          <img className="header__video-player--play" src={PlayIcon} />
+          <img className="header__video-player--fullscreen" src={FullScreen} />
+          <img className="header__video-player--volume" src={VolumeOff} />
+        </div>
       </header>
     </>
   );
