cd5913600a7eb11ecc5101ea4b7ce4278ade12a9
Kyle Ingham <kyleingy@proton.me>
2024-08-22 20:53:07 -0600

    Styled upload page.  Dyanmically responsive at all break-points and matches mock-ups on all screen sizes.

--- src/pages/VideoUploadPage/VideoUploadPage.jsx
+++ src/pages/VideoUploadPage/VideoUploadPage.jsx
@@ -50,58 +50,64 @@ function VideoUploadPage() {
         <div className="upload-video__content">
           <h1 className="upload-video__title">Upload Video</h1>
           <form className="upload-video__form" onSubmit={uploadSubmit}>
-            <div className="upload-video__thumbnail-container">
-              <label
-                className="upload-video__thumbnail-label"
-                htmlFor="thumbnail"
-              >
-                VIDEO THUMBNAIL
-              </label>
-              <img
-                className="upload-video__thumbnail-img"
-                src={thumbnail}
-                id="thumbnail"
-              />
-            </div>
-            <div className="upload-video__input-container">
-              <label
-                className="upload-video__input-title--label"
-                htmlFor="title"
-              >
-                TITLE TO YOUR VIDEO
-              </label>
-              <input
-                className={`upload-video__input-title ${invalidUploadTitle}`}
-                id="title"
-                type="text"
-                placeholder="Add a title to your video"
-                onChange={inputChange}
-              />
-              <label
-                className="upload-video__input-description--label"
-                htmlFor="description"
-              >
-                ADD A VIDEO DESCRIPTION
-              </label>
-              <textarea
-                className={`upload-video__input-description ${invalidUploadDescription}`}
-                id="description"
-                type="text"
-                placeholder="Add a description to your video"
-                onChange={inputChange}
-              />
-            </div>
-            <div className="upload-video__button-container">
-              <div className="upload-video__button-submit-container">
-                <button className="upload-video__button-submit" type="submit">
-                  PUBLISH
-                </button>
-                <img
-                  className="upload-video__button-submit--icon"
-                  src={uploadIcon}
+            <div className="upload-video__form-section">
+              <div className="upload-video__thumbnail-container">
+                <label
+                  className="upload-video__thumbnail-label"
+                  htmlFor="thumbnail"
+                >
+                  VIDEO THUMBNAIL
+                </label>
+                <div className="upload-video__image-container">
+                  <img
+                    className="upload-video__thumbnail-img"
+                    src={thumbnail}
+                    id="thumbnail"
+                  />
+                </div>
+              </div>
+              <div className="upload-video__input-container">
+                <label
+                  className="upload-video__input-title--label"
+                  htmlFor="title"
+                >
+                  TITLE TO YOUR VIDEO
+                </label>
+                <input
+                  className={`upload-video__input-title ${invalidUploadTitle}`}
+                  id="title"
+                  type="text"
+                  placeholder="Add a title to your video"
+                  onChange={inputChange}
+                />
+                <label
+                  className="upload-video__input-description--label"
+                  htmlFor="description"
+                >
+                  ADD A VIDEO DESCRIPTION
+                </label>
+                <textarea
+                  className={`upload-video__input-description ${invalidUploadDescription}`}
+                  id="description"
+                  type="text"
+                  placeholder="Add a description to your video"
+                  onChange={inputChange}
                 />
               </div>
-              <button className="upload-video__button-cancel">CANCEL</button>
+            </div>
+            <div className="upload-video__button-section">
+              <div className="upload-video__button-container">
+                <div className="upload-video__button-submit-container">
+                  <button className="upload-video__button-submit" type="submit">
+                    PUBLISH
+                  </button>
+                  <img
+                    className="upload-video__button-submit--icon"
+                    src={uploadIcon}
+                  />
+                </div>
+                <button className="upload-video__button-cancel">CANCEL</button>
+              </div>
             </div>
           </form>
         </div>
